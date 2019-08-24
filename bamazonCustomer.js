1b05671d25d2ef42100d8fc7545063d44dcb6dcf
Colin <viewtifuljho@hotmail.com>
2019-08-24 14:08:27 -0500

    bamazonCustomer.js complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,52 +21,65 @@ connection.connect(function(err) {
 function showProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        var productArray = [];
-        for (var j = 0; j < res.length; j++) {
-            productArray.push(res[j]);
-        };
-        for (var i = 0; i < productArray.length; i++) {
-            console.log("ID: " + productArray[i].item_id + " || Item: " + productArray[i].product_name + " || Price: $" + productArray[i].price);
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " || Item: " + res[i].product_name + " || Price: $" + res[i].price);
         };
         
         promptUser();
+
+        function promptUser() {
+            inquirer.prompt([
+                {
+                    name: "id",
+                    type: "input",
+                    message: "Please enter the ID of the item you'd like to purchase.",
+                    validate: function(num) {
+                        num = parseInt(num)
+                        return (!isNaN(num) && num <= 10 && num >= 1)
+                    }
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many of the selected item would you like to purchase?",
+                    validate: function(value) {
+                        return (!isNaN(value))
+                    }
+                }
+            ])
+            .then(function(answer) {
+                var selectedId = parseInt(answer.id);
+                var itemPrice = parseInt(res[selectedId].price);
+                var stockQuantity = parseInt(res[selectedId].stock_quantity);
+                var purchaseQuantity = parseInt(answer.quantity);
+                var remainingQuantity = stockQuantity - purchaseQuantity;
+                if (stockQuantity >= purchaseQuantity) {
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                          {
+                            stock_quantity: remainingQuantity
+                          },
+                          {
+                            item_id: selectedId
+                          }
+                        ],
+                        function(error) {
+                          if (error) throw err;
+                          console.log("Purchase complete! Your total cost comes to $" + itemPrice * purchaseQuantity);
+                          showProducts();
+                        }
+                      );
+                }
+        
+                else {
+                    console.log("Sorry, we only have " + stockQuantity + " of that item in stock.")
+                };
+        
+            }).catch(function(err){
+                console.log(err.message)
+            })
+        }
     });
 };
 
-function promptUser() {
-    inquirer.prompt([
-        {
-            name: "id",
-            type: "input",
-            message: "Please enter the ID of the item you'd like to purchase.",
-            validate: function(num) {
-                num = parseInt(num)
-                return (!isNaN(num) && num <= 10 && num >= 1)
-            }
-        },
-        {
-            name: "quantity",
-            type: "input",
-            message: "How many of the selected item would you like to purchase?",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                else {
-                    return false;
-                };
-            }
-        }
-    ])
-    .then(function(answer) {
-        debugger
-        var selectedId = parseInt(answer.id);
-        var purchaseQuantity = parseInt(answer.quantity);
-        console.log(selectedId);
-        console.log(purchaseQuantity);
-
-    }).catch(function(err){
-        console.log(err.message)
-        debugger
-    })
-}
